1d190bb119455dc7545619cce76b53b433129583
unknown <maahboss@yahoo.com>
2018-08-02 09:17:12 +0300

    emitting

--- server/server.js
+++ server/server.js
@@ -14,14 +14,14 @@ app.use(express.static(publicPath));
 io.on('connection',(socket) => {
   console.log('New User connected');
 
-  socket.emit('newMessage',{
-     from: 'Sara',
-     text:'Hey , From Sara',
-     createdAt: 3255
-  });
 
   socket.on('createMessage',(message) =>{
     console.log('createMessage',message);
+    io.emit('newMessage',{
+        from:message.from,
+        text:message.text,
+        createdAt:new Date().getTime()
+    });
     
   });
   
