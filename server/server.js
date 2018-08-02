8052fa5144ad1f63da97e2a12cbd805e88eeff4b
unknown <maahboss@yahoo.com>
2018-08-02 10:17:12 +0300

    broadcasting edit

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -16,28 +17,16 @@ io.on('connection',(socket) => {
 
   //socket.emit from admin text say welcome to the chat app
 
-  socket.emit('newMessage',{
-    from:'Admin',
-    text:'welcome to chat app',
-    createdAt: new Date().getTime()
-  });
+  socket.emit('newMessage',generateMessage('Admin','welcome to Chat app'));
 
   // socket brodcast.emit from admin text that new user has connected
-  socket.broadcast.emit('newMessage',{
-    from:'Admin',
-    text:'New User Joined',
-    createdAt: new Date().getTime()
-  });
+  socket.broadcast.emit('newMessage',generateMessage('Admin','New User is Joined!!'));
 
 
   socket.on('createMessage',(message) =>{
     console.log('createMessage',message);
     
-     io.emit('newMessage',{
-         from:message.from,
-         text:message.text,
-         createdAt:new Date().getTime()
-     });
+     io.emit('newMessage',generateMessage(message.from,message.text));
 
     // you can just send to all exept you
     // socket.broadcast.emit('newMessage',{
